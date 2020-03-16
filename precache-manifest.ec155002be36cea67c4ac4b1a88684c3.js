0c503301f6cb686f1ed7882b366f7b29b1b9b18a
JRM Dev Works <jrmdevworks@gmail.com>
2020-03-16 17:06:23 +0800

    Updates

---
+++ precache-manifest.ec155002be36cea67c4ac4b1a88684c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be3dd8fda89f9bfab4d9ddf55a881a7",
+    "url": "/ligfrontendreactjs/index.html"
+  },
+  {
+    "revision": "cba0eaad94ab30e3fdf5",
+    "url": "/ligfrontendreactjs/static/js/2.cd0928c6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ligfrontendreactjs/static/js/2.cd0928c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76494fea70efecfa6564",
+    "url": "/ligfrontendreactjs/static/js/main.faca39f9.chunk.js"
+  },
+  {
+    "revision": "b90c07cd56abbc6368ec",
+    "url": "/ligfrontendreactjs/static/js/runtime-main.d1074f69.js"
+  }
+]);
