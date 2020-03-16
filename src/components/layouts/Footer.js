97a7b79530d6b61846cb4565f4081d54ccfd1da8
JRM Dev Works <jrmdevworks@gmail.com>
2020-03-16 17:32:45 +0800

    fixed footer link on deploy

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
         <div className="footer-container">
             <div className="footer clearfix">
                 <div className="links float-left">
-                    <Link to="sample-page">運営会社</Link>  &nbsp; &nbsp; 
-                    <Link to="sample-page">個人情報保護について</Link>                   
+                    <Link to="/sample-page">運営会社</Link>  &nbsp; &nbsp; 
+                    <Link to="/sample-page">個人情報保護について</Link>                   
                 </div>
                 <div className="copyright float-right">&copy;2016 Sample</div>
             </div>	
